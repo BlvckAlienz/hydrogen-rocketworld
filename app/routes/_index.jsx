9ee237e467a23328faf6ba644008a4d7db04361f
BlvckAlienz <131600507+BlvckAlienz@users.noreply.github.com>
2023-05-14 00:07:09 +0100

    Add files via upload

---
+++ app/routes/_index.jsx
@@ -0,0 +1,15 @@
+export function meta() {
+    return [
+      {title: 'Blvck Market'},
+      {description: 'A custom storefront powered by Hydrogen'},
+    ];
+  }
+  
+  export default function Index() {
+    return (
+      <div>
+        <h3>Welcome to Your Local Blvck Market!</h3>
+      </div>
+    );
+  }
+  
